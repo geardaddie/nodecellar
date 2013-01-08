143d169602bd1cda140bf62aa25d096aaaa4e66f
John Shields <john@agilitec-inc.com>
2013-01-07 21:03:59 -0600

    express running

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var http = require('http');
+var express = require('express');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
+var app = express();
 
-console.log('Server running at http://127.0.0.1:3000/');
+app.get('/wines', function(req, res) {
+  res.send([{name: 'wine1'}, {name: 'wine2'}]);
+});
+
+app.get('/wines/:id', function(req, res) {
+  res.send({id: req.param.id, name: "The Name", description: "description"});
+});
+
+app.listen(3000);
+
+console.log('Listening on port 3000....');
 
